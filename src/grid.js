d8446884ccfc5b9cd69feec99260839d0c3bf5de
Eric Sorum <ejsorum@gmail.com>
2022-04-04 10:33:43 -0500

    Initial commit.

---
+++ src/grid.js
@@ -0,0 +1,11 @@
+//import birds from './birds.jpg'
+//import lake from './lake.jpg'
+import logo from './logo.svg'
+
+function grid() {
+    let img = document.createElement('img')
+    img.src = logo
+    return img
+}
+
+export default grid
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/app.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      static: path.resolve(__dirname, 'dist'),
+      port: 8080,
+      open: true,
+      hot: true,
+      watchFiles: [path.resolve(__dirname, 'src')]
+    },
+    module: {
+        rules: [
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource'},
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Gallery',
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/template.html')
+        }),
+    ],
+}
