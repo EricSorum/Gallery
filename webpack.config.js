2320369edd9d69fea7c0fde798865b2715d88465
Eric Sorum <ejsorum@gmail.com>
2022-04-11 09:54:43 -0500

    First imgloader commit.  Dysfunctional.

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,29 @@ module.exports = {
          {
            test: /\.(png|svg|jpg|jpeg|gif)$/i,
            type: 'asset/resource',
+           use: [
+             'file-loader',
+             {
+               loader: 'image-webpack-loader',
+               options: {
+                 bypassOnDebug: true,
+                 disable: true,
+               },
+               optipng: {
+                 enabled: false,
+               },
+               pnquant: {
+                 quality: [0.65, 0.90],
+                 speed: 4
+               },
+               gifsicle: {
+                 interlaced: false,
+               },
+               webp: {
+                 quality: 75
+               }
+             }
+           ]
          },
         ],
       },
